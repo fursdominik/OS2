8cd3533cb5dad6846bd516995884592bfb7b436c
FursDominik <fdsfurs007@gmail.com>
2022-11-12 12:42:12 +0300

    Change way of output

--- OS_2/OS_2/OS_2.cpp
+++ OS_2/OS_2/OS_2.cpp
@@ -1,7 +1,105 @@
 ﻿#include <unistd.h>
 #include <sys/wait.h>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char** argv) {
+
+	printf("Дочерний 0_0 %d\n", getpid());
+	printf("Родительский %d\n", getppid());
+
+	pid_t pid_1_2;
+
+	if ((pid_1_2 = fork()) == 0) {
+
+		printf("Процесс %d породил процесс %d\n", getppid(), getpid());
+		printf("Дочерний 1_2 %d\n", getpid());
+		printf("Родительский %d\n", getppid());
+		printf("Дочерний 1_2 %d завершает работу\n", getpid());
+
+		exit(0);
+	}
+
+	wait(0);
+
+	pid_t pid_1_3;
+
+	if ((pid_1_3 = fork()) == 0) {
+
+		printf("Процесс %d породил процесс %d\n", getppid(), getpid());
+		printf("Дочерний 1_3 %d\n", getpid());
+		printf("Родительский %d\n", getppid());
+
+		pid_t pid_3_5;
+
+		if ((pid_3_5 = fork()) == 0) {
+
+			printf("Процесс %d породил процесс %d\n", getppid(), getpid());
+			printf("Дочерний 3_5 %d\n", getpid());
+			printf("Родительский %d\n", getppid());
+
+			pid_t pid_5_7;
+
+			if ((pid_5_7 = fork()) == 0) {
+
+				printf("Процесс %d породил процесс %d\n", getppid(), getpid());
+				printf("Дочерний 5_7 %d\n", getpid());
+				printf("Родительский %d\n", getppid());
+				printf("Дочерний 5_7 %d завершает работу\n", getpid());
+
+				exit(0);
+			}
+
+			wait(0);
+
+			printf("Дочерний 3_5 %d завершает работу\n", getpid());
+			exit(0);
+		}
+
+		wait(0);
+
+		pid_t pid_3_6;
+
+		if ((pid_3_6 = fork()) == 0) {
+
+			printf("Процесс %d породил процесс %d\n", getppid(), getpid());
+			printf("Дочерний 3_6 %d\n", getpid());
+			printf("Родительский %d\n", getppid());
+
+			printf("Дочерний 3_6 %d выполняет команду pwd\n", getpid());
+			execvp("pwd", argv);
+
+			printf("Дочерний 3_6 %d завершает работу\n", getpid());
+			exit(0);
+		}
+
+		wait(0);
+
+		printf("Дочерний 1_3 %d завершает работу\n", getpid());
+		exit(0);
+	}
+
+	wait(0);
+
+	pid_t pid_1_4;
+
+	if ((pid_1_4 = fork()) == 0) {
+
+		printf("Процесс %d породил процесс %d\n", getppid(), getpid());
+		printf("Дочерний 1_4 %d\n", getpid());
+		printf("Родительский %d\n", getppid());
+		printf("Дочерний 1_4 %d завершает работу\n", getpid());
+
+		exit(0);
+	}
+
+	wait(0);
+
+	printf("Дочерний 0_0 %d завершает работу\n", getpid());
+	return 0;
+}
+/*
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -9,9 +107,9 @@ int main(int argc, char** argv) {
 	cout << "Дочерний 0.0  " << getpid() << '\n';
 	cout << "Родительский" << getppid() << '\n';
 
-	pid_t pid_1_1;
+	pid_t pid_1_2;
 
-	if ((pid_1_1 = fork()) == 0) {
+	if ((pid_1_2 = fork()) == 0) {
 
 		cout << "Процесс " << getppid() << " породил процесс" << getpid() << '\n';
 		cout << "Дочерний 1.2  " << getpid() << '\n';
@@ -22,23 +120,23 @@ int main(int argc, char** argv) {
 	}
 	wait(0);
 
-	pid_t pid_1_2;
+	pid_t pid_1_3;
 
-	if ((pid_1_2 = fork()) == 0) {
+	if ((pid_1_3 = fork()) == 0) {
 
 		cout << "Процесс " << getppid() << " породил процесс" << getpid() << '\n';
 		cout << "Дочерний 1.3  " << getpid() << '\n';
 		cout << "Родительский" << getppid() << '\n';
 
-		pid_t pid_2_4;
-		if ((pid_2_4 = fork()) == 0) {
+		pid_t pid_3_5;
+		if ((pid_3_5 = fork()) == 0) {
 
 			cout << "Процесс " << getppid() << " породил процесс" << getpid() << '\n';
 			cout << "Дочерний 3.5  " << getpid() << '\n';
 			cout << "Родительский" << getppid() << '\n';
 
-			pid_t pid_3_6;
-			if ((pid_3_6 = fork()) == 0) {
+			pid_t pid_5_7;
+			if ((pid_5_7 = fork()) == 0) {
 
 				cout << "Процесс " << getppid() << " породил процесс" << getpid() << '\n';
 				cout << "Дочерний 5.7  " << getpid() << '\n';
@@ -91,103 +189,6 @@ int main(int argc, char** argv) {
 
 	cout << "Дочерний 0.0 " << getpid() << " завершает работу" << '\n';
 
-	return 0;
-}
-/*int main(int argc, char** argv) {
-
-	printf("******НАЧАЛО******\n");
-	printf("Дочерний 0_1 %d\n", getpid());
-	printf("Родительский %d\n", getppid());
-	printf("******************\n");
-
-	pid_t pid_1_1;
-
-	if ((pid_1_1 = fork()) == 0) {
-
-		printf("Процесс %d породил процесс %d\n", getppid(), getpid());
-		printf("Дочерний 1_2 %d\n", getpid());
-		printf("Родительский %d\n", getppid());
-		printf("Дочерний 1_2 %d завершает работу\n", getpid());
-
-		exit(0);
-	}
-
-	wait(0);
-
-	pid_t pid_1_2;
-
-	if ((pid_1_2 = fork()) == 0) {
-
-		printf("Процесс %d породил процесс %d\n", getppid(), getpid());
-		printf("Дочерний 1_3 %d\n", getpid());
-		printf("Родительский %d\n", getppid());
-
-		pid_t pid_2_4;
-
-		if ((pid_2_4 = fork()) == 0) {
-
-			printf("Процесс %d породил процесс %d\n", getppid(), getpid());
-			printf("Дочерний 2_5 %d\n", getpid());
-			printf("Родительский %d\n", getppid());
-
-			pid_t pid_3_6;
-
-			if ((pid_3_6 = fork()) == 0) {
-
-				printf("Процесс %d породил процесс %d\n", getppid(), getpid());
-				printf("Дочерний 2_7 %d\n", getpid());
-				printf("Родительский %d\n", getppid());
-				printf("Дочерний 2_7 %d завершает работу\n", getpid());
-
-				exit(0);
-			}
-
-			wait(0);
-
-			printf("Дочерний 2_5 %d завершает работу\n", getpid());
-			exit(0);
-		}
-
-		wait(0);
-
-		pid_t pid_2_5;
-
-		if ((pid_2_5 = fork()) == 0) {
-
-			printf("Процесс %d породил процесс %d\n", getppid(), getpid());
-			printf("Дочерний 2_6 %d\n", getpid());
-			printf("Родительский %d\n", getppid());
-
-			printf("Дочерний 2_6 %d executes order 66\n", getpid());
-			execvp("pwd", argv);
-
-			printf("Дочерний 2_6 %d завершает работу\n", getpid());
-			exit(0);
-		}
-
-		wait(0);
-
-		printf("Дочерний 1_3 %d завершает работу\n", getpid());
-		exit(0);
-	}
-
-	wait(0);
-
-	pid_t pid_1_3;
-
-	if ((pid_1_3 = fork()) == 0) {
-
-		printf("Процесс %d породил процесс %d\n", getppid(), getpid());
-		printf("Дочерний 1_4 %d\n", getpid());
-		printf("Родительский %d\n", getppid());
-		printf("Дочерний 1_4 %d завершает работу\n", getpid());
-
-		exit(0);
-	}
-
-	wait(0);
-
-	printf("Дочерний 0_1 %d завершает работу\n", getpid());
 	return 0;
 }
 */
